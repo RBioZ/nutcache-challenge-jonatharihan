4ad2418ff52fbd19c97cd94aaf9f2ee832c46214
RBioZ <jonatha_rihan@hotmail.com>
2021-03-05 10:21:35 -0300

    :sparkles: feat: Index Theme Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import Main from './pages/Main';
+import light from './assets/styles/themes/light';
 
 import GlobalStyle from './assets/styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Main />
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <Main />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
