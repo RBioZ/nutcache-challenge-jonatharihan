bd935b78acb403ffef15ab4153ca01469cbe6691
RBioZ <jonatha_rihan@hotmail.com>
2021-03-05 14:30:31 -0300

    :sparkles: feat: Popup

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import * as S from './styles';
 
 const Popup: React.FC = () => {
   return (
-    <div className="popup">
-      <div className="popup\_inner">
-        <h1>Any Text</h1>
-        <button type="button" onClick={() => console.log('Hello')}>
-          close me
-        </button>
-      </div>
-    </div>
+    <S.Background>
+      <S.Container>
+        <form>
+          <h3>Cadastro de Funcionário</h3>
+
+          <input name="name" placeholder="Nome Completo" />
+          <input name="email" placeholder="E-mail" />
+          <input name="taxid" placeholder="CPF" />
+
+          <div>
+            <div className="wrapper">
+              <p>Data de Nascimento</p>
+              <input type="date" id="birthday" name="birthday" />
+            </div>
+
+            <div className="wrapper">
+              <p>Data de Adimissão</p>
+              <input type="date" id="admission" name="admission" />
+            </div>
+          </div>
+
+          <div>
+            <div className="wrapper">
+              <p>Gênero</p>
+              <select name="gender" id="gender">
+                <option value="male">Masculino</option>
+                <option value="female">Feminino</option>
+                <option value="none">Outro</option>
+              </select>
+            </div>
+
+            <div className="wrapper">
+              <p>Equipe</p>
+              <select name="team" id="gender">
+                <option value=""> </option>
+                <option value="backend">Backend</option>
+                <option value="frontend">Frontend</option>
+                <option value="mobile">Mobile</option>
+              </select>
+            </div>
+          </div>
+
+          <button type="button">Cadastrar</button>
+        </form>
+      </S.Container>
+    </S.Background>
   );
 };
 
--- src/components/Popup/styles.ts
+++ src/components/Popup/styles.ts
@@ -1,3 +1,80 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Background = styled.div`
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 100vmax;
+`;
+
+export const Container = styled.div`
+  width: 50%;
+
+  margin: auto;
+  margin-top: 5%;
+
+  background-color: #fff;
+  border-radius: 5px;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    padding: 2rem 3rem;
+
+    div {
+      display: flex;
+      justify-content: space-between;
+      .wrapper {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+      }
+      input {
+        width: 99%;
+      }
+    }
+
+    input {
+      margin-top: 1rem;
+      border: none;
+      background-color: rgba(0, 0, 0, 0.07);
+      height: 4rem;
+      padding-left: 1rem;
+    }
+
+    select {
+      width: 99%;
+      height: 4rem;
+      background-color: rgba(0, 0, 0, 0.07);
+      padding-left: 1rem;
+      border: none;
+      font-size: 2rem;
+    }
+
+    p {
+      margin-top: 2rem;
+    }
+
+    button {
+      background-color: ${(props) => props.theme.colors.bg_success};
+      color: ${(props) => props.theme.colors.bg_light};
+      height: 4rem;
+      margin-top: 4rem;
+      border: none;
+
+      :hover {
+        cursor: pointer;
+        opacity: 0.8;
+        transition: 500ms;
+      }
+      :focus {
+        outline: 0;
+      }
+    }
+  }
+`;
