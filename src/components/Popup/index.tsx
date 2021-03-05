16ed8e470de9e9c2deade40e2c81484bda32026b
RBioZ <jonatha_rihan@hotmail.com>
2021-03-05 10:21:52 -0300

    :spakrles: feat: Popup

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const Popup: React.FC = () => <div />;
+const Popup: React.FC = () => {
+  return (
+    <div className="popup">
+      <div className="popup\_inner">
+        <h1>Any Text</h1>
+        <button type="button" onClick={() => console.log('Hello')}>
+          close me
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Popup;
